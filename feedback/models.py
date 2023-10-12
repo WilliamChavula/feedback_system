5c6ee524576ef7e7442234918b7a46f93c98bddc
williamchavula <rumbani@gmail.com>
2023-10-12 11:08:13 -0500

    create AppUser model

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class AppUser(AbstractUser):
+
+    email = models.EmailField(
+        _("email address"),
+        help_text="Required. Enter a valid email address",
+        unique=True,
+        blank=True,
+    )
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return f"{self.email}"
