5af23298937f1136c905688a073bbb3555cd9954
williamchavula <rumbani@gmail.com>
2023-10-12 00:31:27 -0500

    initialize tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./templates/**/*.html", "./**/templates/**/*.html", "./**/*.py"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
