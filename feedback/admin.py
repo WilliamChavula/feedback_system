a4af08ceff3f2eb73e537a39100ee8306a872f01
williamchavula <rumbani@gmail.com>
2023-10-18 10:55:20 -0500

    register models in admin site.

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Feedback
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = (
+        "course_name",
+        "course_code",
+        "professor",
+        "feedback_text",
+        "date_added",
+    )
