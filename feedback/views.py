f05913d4df5d21a34aff43d4f24d767e2a6c3ade
williamchavula <rumbani@gmail.com>
2023-10-19 12:55:54 -0500

    add code to capture student feedback and save in db

--- feedback/views.py
+++ feedback/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from account.models import AppUser
 from .forms import AddFeedbackForm
 from .models import Feedback
 
@@ -12,6 +13,15 @@ class FeedbackListView(ListView):
     template_name = "feedback/list.html"
 
 
-class AddFeedbackView(LoginRequiredMixin, FormView):
+class AddFeedbackView(LoginRequiredMixin, CreateView):
     form_class = AddFeedbackForm
     success_url = reverse_lazy("feedback:list")
+    template_name = "feedback/add.html"
+
+    def form_valid(self, form):
+        feedback = form.save(commit=False)
+        feedback.student = self.request.user
+
+        feedback.save()
+
+        return super(AddFeedbackView, self).form_valid(form)
