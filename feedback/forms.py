76392ba70572bb5b7bcf9078ca4718c70ac49ace
williamchavula <rumbani@gmail.com>
2023-10-19 12:55:26 -0500

    add styles to input fields

--- feedback/forms.py
+++ feedback/forms.py
@@ -12,3 +12,45 @@ class AddFeedbackForm(forms.ModelForm):
             "professor",
             "feedback_text",
         )
+
+    course_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Name of Course",
+                "class": "w-full border border-gray-300 rounded-md py-2 px-3 my-3 "
+                         "focus:outline-none focus:border-blue-700",
+            }
+        ),
+    )
+
+    course_code = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Course Code",
+                "class": "w-full border border-gray-300 rounded-md py-2 px-3 my-1 "
+                         "focus:outline-none focus:border-blue-700",
+            }
+        ),
+    )
+
+    professor = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Professor",
+                "class": "w-full border border-gray-300 rounded-md py-2 px-3 my-1 "
+                         "focus:outline-none focus:border-blue-700",
+            }
+        ),
+        help_text="Who teaches this course?",
+    )
+
+    feedback_text = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Write your review",
+                "class": "w-full border border-gray-300 rounded-md py-2 px-3 my-1 "
+                         "focus:outline-none focus:border-blue-700",
+            }
+        ),
+        help_text="Obscene language is not accepted. Please be kind, polite, and constructive in your review.",
+    )
