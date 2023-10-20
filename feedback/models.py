c62fc964b6902e0e8dc1382c0b670841c891366a
williamchavula <rumbani@gmail.com>
2023-10-20 16:22:04 -0500

    add extra models for Instructor and Course. This will allow for autofill options for similar courses

--- feedback/models.py
+++ feedback/models.py
@@ -22,3 +22,37 @@ class Feedback(models.Model):
             f"course_name: {self.course_name} - course_code: {self.course_code}) "
             f"taught by professor: {self.professor}"
         )
+
+
+class Course(models.Model):
+    course_id = models.CharField(
+        max_length=8, primary_key=True, help_text="The Course code"
+    )
+    course_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.course_name
+
+
+class InstructorTitle(models.TextChoices):
+    TA = ("TA", "TA")
+    Professor = ("Professor", "professor")
+    AsstProf = ("Asst. Professor", "asst. professor")
+    Dr = ("Dr", "dr")
+    Mr = ("Mr", "mr")
+    Miss = ("Miss", "miss")
+
+
+class Instructor(models.Model):
+    name = models.CharField(
+        max_length=255,
+        help_text="Name of the person teaching the course. Could the the Professor or TA",
+    )
+    instructor_type = models.CharField(
+        max_length=20,
+        choices=InstructorTitle.choices,
+        default=InstructorTitle.Professor,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.instructor_type} {self.name}"
