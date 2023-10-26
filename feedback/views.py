647ab6cdd0b32c7615595c32114118b71e7ac3e1
williamchavula <rumbani@gmail.com>
2023-10-25 23:15:14 -0500

    add dependency to CensorService to check for profanity in feedback.

--- feedback/views.py
+++ feedback/views.py
@@ -3,10 +3,9 @@ from django.views.generic import ListView, CreateView
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from better_profanity import profanity
-
 from .forms import AddFeedbackForm
 from .models import Feedback
+from .service import CensorService, ICensorService
 
 
 class FeedbackListView(ListView):
@@ -19,19 +18,23 @@ class AddFeedbackView(LoginRequiredMixin, CreateView):
     form_class = AddFeedbackForm
     success_url = reverse_lazy("feedback:list")
     template_name = "feedback/add.html"
+    service: ICensorService = CensorService()
 
     def form_valid(self, form):
-        profanity.load_censor_words()
         feedback = form.save(commit=False)
         feedback.student = self.request.user
 
-        if profanity.contains_profanity(feedback.feedback_text):
+        if self.service.has_profane(feedback.feedback_text):
             messages.error(
                 self.request,
-                "The text may contain words flagged as being profane. Feedback not saved!",
+                "The text may contain words flagged as being profane. This feedback was not saved!",
             )
 
             return super(AddFeedbackView, self).form_invalid(form)
 
         feedback.save()
         return super(AddFeedbackView, self).form_valid(form)
+
+
+# Todo:
+#   - Add graphics to the templates.
