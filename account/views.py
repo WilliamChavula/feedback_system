2afac8351ff6bc790c05a1f8d9f64526eb3dc77a
williamchavula <rumbani@gmail.com>
2023-10-25 23:14:32 -0500

    override dispatch method and add redirect to add/ route if user is already authenticated, else proceed per usual

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -8,3 +9,9 @@ class UserRegistrationView(CreateView):
     form_class = RegistrationForm
     success_url = reverse_lazy("account:login")
     template_name = "account/signup.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("feedback:add")
+
+        return super(UserRegistrationView, self).dispatch(request, *args, **kwargs)
